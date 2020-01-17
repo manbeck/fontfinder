7d7ec5e35974607cf4776e246296bfe684645fe8
Glitch (savory-hoodie) <none>
2020-01-17 08:14:53 +0000

    🙈🙉 Updated with Glitch

---
+++ public/client.js
@@ -0,0 +1,130 @@
+// client-side js
+// run by the browser each time your view template referencing it is loaded
+
+console.log("hello world :o");
+
+var verbs, nouns, adjectives, adverbs, preposition;
+nouns = [
+  "bird",
+  "clock",
+  "boy",
+  "plastic",
+  "duck",
+  "teacher",
+  "old lady",
+  "professor",
+  "hamster",
+  "dog"
+];
+verbs = [
+  "kicked",
+  "ran",
+  "flew",
+  "dodged",
+  "sliced",
+  "rolled",
+  "died",
+  "breathed",
+  "slept",
+  "killed"
+];
+adjectives = [
+  "beautiful",
+  "lazy",
+  "professional",
+  "lovely",
+  "dumb",
+  "rough",
+  "soft",
+  "hot",
+  "vibrating",
+  "slimy"
+];
+adverbs = [
+  "slowly",
+  "elegantly",
+  "precisely",
+  "quickly",
+  "sadly",
+  "humbly",
+  "proudly",
+  "shockingly",
+  "calmly",
+  "passionately"
+];
+preposition = [
+  "down",
+  "into",
+  "up",
+  "on",
+  "upon",
+  "below",
+  "above",
+  "through",
+  "across",
+  "towards"
+];
+
+function randGen() {
+  return Math.floor(Math.random() * 5);
+}
+
+function sentence() {
+  var rand1 = Math.floor(Math.random() * 10);
+  var rand2 = Math.floor(Math.random() * 10);
+  var rand3 = Math.floor(Math.random() * 10);
+  var rand4 = Math.floor(Math.random() * 10);
+  var rand5 = Math.floor(Math.random() * 10);
+  var rand6 = Math.floor(Math.random() * 10);
+  //                var randCol = [rand1,rand2,rand3,rand4,rand5];
+  //                var i = randGen();
+  var content =
+    "The " +
+    adjectives[rand1] +
+    " " +
+    nouns[rand2] +
+    " " +
+    adverbs[rand3] +
+    " " +
+    verbs[rand4] +
+    " because some " +
+    nouns[rand1] +
+    " " +
+    adverbs[rand1] +
+    " " +
+    verbs[rand1] +
+    " " +
+    preposition[rand1] +
+    " a " +
+    adjectives[rand2] +
+    " " +
+    nouns[rand5] +
+    " which, became a " +
+    adjectives[rand3] +
+    ", " +
+    adjectives[rand4] +
+    " " +
+    nouns[rand6] +
+    ".";
+
+  document.getElementsByClassName("sentence").innerHTML = "&quot;" + content + "&quot;";
+}
+sentence();
+
+(function($, window, document, undefined) {
+  "use strict";
+
+  $(function() {
+    $("#mobileMenu").hide();
+    $(".toggleMobile").click(function() {
+      $(this).toggleClass("active");
+      $("#mobileMenu").slideToggle(500);
+    });
+  });
+  $(window).on("resize", function() {
+    if ($(this).width() > 500) {
+      $("#mobileMenu").hide();
+      $(".toggleMobile").removeClass("active");
+    }
+  });
+})(jQuery, window, document);
